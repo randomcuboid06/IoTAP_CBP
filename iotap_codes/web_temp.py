07203e1020498864470d1237fbdd06712b490fab
randomcuboid06 <112553686+randomcuboid06@users.noreply.github.com>
2023-12-28 22:27:34 +0530

    Create web_temp.py

---
+++ iotap_codes/web_temp.py
@@ -0,0 +1,14 @@
+# IMPORTANT: connect temp sensor to GPIO 4
+from flask import Flask
+import w1thermsensor
+
+sensor = w1thermsensor()
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    temp = sensor.get_temperature()
+    return 'temp: {} degree C'.format(temp)
+    # don't forget to reload the page to get new readings
+
+app.run(debug = True)
