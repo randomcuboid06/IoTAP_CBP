899f4bb2dc5e2093eaa164686100037562449222
randomcuboid06 <112553686+randomcuboid06@users.noreply.github.com>
2023-12-29 06:59:36 +0530

    Update led_web.py workingg

--- iotap_codes/led_web.py
+++ iotap_codes/led_web.py
@@ -1,27 +1,26 @@
-# NOT WORKING SHOULD SEE
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 import RPi.GPIO as gp
 
 gp.setmode(gp.BCM)
 gp.setwarnings(False)
-gp.setup(26, gp.OUT)
-
+led=26
+gp.setup(led, gp.OUT)
 
 app = Flask(__name__)
 
-# templates ane folder lo index.html ani create chey
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def home():
-  if request.method == 'POST':
-    state = request.form['state']
-    if state == 'on':
-      gp.output(26, gp.HIGH)
-    elif state == 'off':
-      gp.output(26, gp.LOW)
+    if request.method == 'POST':
+        state = request.form['led']
+        if state == 'on':
+            gp.output(led, gp.HIGH)
+        elif state == 'off':
+            gp.output(led, gp.LOW)
+        else:
+            print('Enter correct state!!')
+        return render_template('index.html')
     else:
-      print('Wrong input!')
-    
-  else:
-    return render_template('index.html')
-
-app.run(debug = True)
+        return render_template('index.html')
+  
+if __name__ == '__main__':
+    app.run(debug=True)
